13fe19f78eceaf2640048e1aae698803e80cbf55
shivampawar777 <pawarshivam826@gmail.com>
2025-04-27 16:28:01 +0530

    Production changes

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, jsonify, request
-from database import show_jobs, show_jobdetails, job_application
+from flask import Flask, render_template, request
+from database import show_jobs, show_job_details, job_application
+
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home_page():
@@ -15,28 +17,26 @@ def jobs_page():
     return render_template('home.html', jobs=jobs)
 
 
-
 @app.route("/job/<id>")
 def jobdetails_page(id):
-    job = show_jobdetails(id)
+    job = show_job_details(id)
   
     if not job:
         return "Not Found", 404
-  
+    
     return render_template('jobpage.html', job=job)
 
 
-
-@app.route("/job/<id>/apply", methods=['post'])
+@app.route("/job/<id>/apply", methods=['POST'])
 def apply_page(id):
     data = request.form
-    job = load_job_from_db(id)
-    add_application_to_db(id, data)
+    job = show_job_details(id)
+
+    job_application(id, data)
+    
     return render_template('application_submitted.html', application=data, job=job)
 
 
 
-
-
-if __name__ == '__main__':
-    app.run(host='localhost', debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False)
--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine, text
+import os
 
 
-engine = create_engine("mysql+pymysql://root:root@localhost/test1?charset=utf8mb4")
+db_connection_string = os.environ.get('DB_CONNECTION_STRING', 'default_value')
+engine = create_engine(db_connection_string)
 
 
 def show_jobs():
@@ -14,8 +16,7 @@ def show_jobs():
         return jobs
 
 
-
-def show_jobdetails(id):
+def show_job_details(id):
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), {'val':id})
         rows = result.all()
@@ -26,9 +27,13 @@ def show_jobdetails(id):
             return dict(rows[0]._mapping)
 
 
-
 def job_application(job_id, data):
+    values = {'job_id':job_id, 'full_name':data['full_name'], 'email':data['email'], 'linkedin_url':data['linkedin_url'], 'education':data['education'], 'work_experience':data['work_experience'],'resume_url' : data['resume_url']}
+    
     with engine.connect() as conn:
-        query = text("INSERT INTO applications(job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+        query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
 
-        conn.execute(query, job_id=job_id, full_name=data['full_name'], email=data['email'], linkedin_url=data['linkedin_url'], education=data['education'], work_experience=data['work_experience'], resume_url=data['resume_url'])
+        conn.execute(query, values)
+        conn.commit()
+
+
